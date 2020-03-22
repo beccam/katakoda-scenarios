9125d3bcee52064c69c2a14069ea88a9b60114eb
Rebecca Mills <beccamills86@gmail.com>
2020-03-22 17:07:54 -0230

    stuff

--- java/background.sh
+++ java/background.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
 mkdir sunshine
+mkdir sunshine/srcS
 wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 tar xzf apache-cassandra-3.11.6-bin.tar.gz
 sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-3.11.6/conf/cassandra.yaml
