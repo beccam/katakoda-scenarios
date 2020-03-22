8b68052eb0706ad77f80657b9426f1a9571b0a7a
Rebecca Mills <beccamills86@gmail.com>
2020-03-22 20:56:58 -0230

    stuff

--- java/background.sh
+++ java/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"
 mkdir sunshine
 mkdir sunshine/src
 mkdir sunshine/src/main
