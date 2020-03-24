4d78491a2fb9eddd14c2ec11ceba92a1cb1c3f22
Rebecca Mills <beccamills86@gmail.com>
2020-03-24 09:50:53 -0230

    stuff

--- cql-quickstart/background.sh
+++ cql-quickstart/background.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-mkdir ~/.cassandra
-cd ~/.cassandra
-unzip secure-connect-database_name.zip
-wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 tar xzf apache-cassandra-3.11.6-bin.tar.gz
 sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-3.11.6/conf/cassandra.yaml
 apache-cassandra-3.11.6/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" apache-cassandra-3.11.6/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
+mkdir ~/.cassandra
+cd ~/.cassandra
+unzip secure-connect-database_name.zip
+wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 echo "done" >> /opt/katacoda-background-finished
