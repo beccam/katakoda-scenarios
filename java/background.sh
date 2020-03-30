28dfa5c22208a38c5566646723c5d414b28ba2ad
Rebecca Mills <beccamills86@gmail.com>
2020-03-30 16:19:39 -0230

    stuff

--- java/background.sh
+++ java/background.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >>~/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
 mkdir quickstart
 mkdir quickstart/src
 mkdir quickstart/src/main
