022b0013ac474c1bcadb5d3d35cdf9ef87ac6895
Rebecca Mills <beccamills86@gmail.com>
2020-03-31 14:48:21 -0230

    added csharp quickstart

---
+++ csharp/assets/Program.cs
@@ -0,0 +1,64 @@
+using Cassandra;
+using System.Linq;
+using System;
+
+namespace QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // TO DO: Fill in your own contact point
+            Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+            ISession session = cluster.Connect("demo");
+
+            SetUser(session, "Jones", 35, "Austin", "bob@example.com", "Bob");
+
+            GetUser(session, "Jones");
+
+            UpdateUser(session, 36, "Jones");
+
+            GetUser(session, "Jones");
+
+            DeleteUser(session, "Jones");
+<<<<<<< HEAD
+
+=======
+            
+>>>>>>> 8906a2ad995e108d15ee164d9408b5ee8866c460
+            cluster.Dispose();
+
+        }
+
+        private static void SetUser(ISession session, String lastname, int age, String city, String email, String firstname)
+        {
+
+            //TO DO: execute SimpleStatement that inserts one user into the table
+
+        }
+
+        private static void GetUser(ISession session, String lastname)
+        {
+
+            //TO DO: execute SimpleStatement that retrieves one user from the table
+
+            //TO DO: print firstname and age of user
+
+        }
+
+        public static void UpdateUser(ISession session, int age, String lastname)
+        {
+
+            //TO DO: execute SimpleStatement that updates the age of one user
+
+        }
+
+        public static void DeleteUser(ISession session, String lastname)
+        {
+
+            //TO DO: execute SimpleStatement that deletes one user from the table
+
+        }
+
+    }
+}
