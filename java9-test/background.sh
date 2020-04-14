7912357bd62fc93fa457cdcae8cf666ff5c184ef
Rebecca Mills <beccamills86@gmail.com>
2020-04-14 15:26:55 -0230

    added java9-test

--- java9-test/background.sh
+++ java9-test/background.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
-tar xzf apache-cassandra-3.11.6-bin.tar.gz
-sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-3.11.6/conf/cassandra.yaml
+yes | sudo apt-get purge openjdk-\*
+yes | sudo apt install openjdk-9-jre
+export JAVA_HOME="/usr/lib/jvm/java-9-openjdk-amd64/bin/java"
+wget https://github.com/apache/cassandra/archive/apache-cassandra-4.0-alpha3-bin.tar.gz
+tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
+sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-4.0-alpha3/conf/cassandra.yaml
 apache-cassandra-3.11.6/bin/cassandra -R
-while [ `grep "Starting listening for CQL clients" apache-cassandra-3.11.6/logs/system.log | wc -l` -lt 1 ]; do
+while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha3/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
 echo "done" >> /opt/katacoda-background-finished
