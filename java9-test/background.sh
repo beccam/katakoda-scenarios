5c01304040753f239c16baf1f15c5a746a750c6d
Rebecca Mills <beccamills86@gmail.com>
2020-04-14 15:27:22 -0230

    added java9-test

--- java9-test/background.sh
+++ java9-test/background.sh
@@ -5,7 +5,7 @@ export JAVA_HOME="/usr/lib/jvm/java-9-openjdk-amd64/bin/java"
 wget https://github.com/apache/cassandra/archive/apache-cassandra-4.0-alpha3-bin.tar.gz
 tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
 sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-4.0-alpha3/conf/cassandra.yaml
-apache-cassandra-3.11.6/bin/cassandra -R
+apache-cassandra-4.0-alpha3/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha3/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
