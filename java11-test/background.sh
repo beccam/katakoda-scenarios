303005f1575532f1d9fdeeabbaf150566aa5ae57
Rebecca Mills <beccamills86@gmail.com>
2020-04-15 15:21:31 -0230

    updated nodejs

--- java11-test/background.sh
+++ java11-test/background.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-yes | sudo apt-get purge openjdk-\*
-yes | sudo apt-get install openjdk-11-jre openjdk-11-jdk
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
 wget https://downloads.apache.org/cassandra/4.0-alpha3/apache-cassandra-4.0-alpha3-bin.tar.gz
 tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
 sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-4.0-alpha3/conf/cassandra.yaml
