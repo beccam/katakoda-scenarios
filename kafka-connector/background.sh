c7f6b70869cfedd3c259fef72807ef0f37fb2d82
Rebecca Mills <beccamills86@gmail.com>
2020-04-22 11:04:01 -0230

    updated kafka sceanrio

--- kafka-connector/background.sh
+++ kafka-connector/background.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+wget http://apache.spinellicreations.com/kafka/2.2.2/kafka_2.11-2.2.2.tgz
+mkdir kafka; tar -xzf kafka_2.11-1.1.0.tgz -C kafka --strip-components=1
+wget https://downloads.datastax.com/enterprise/dse-6.8.tar.gz
+tar xzf dse-6.8.tar.gz
+dse-6.8/bin/cassandra -R
+echo "done" >> /opt/katacoda-background-finished
