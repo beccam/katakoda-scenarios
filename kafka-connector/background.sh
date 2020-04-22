67d50c1b7e91e84426f675ff4c8e05dd02090421
Rebecca Mills <beccamills86@gmail.com>
2020-04-22 20:39:04 -0230

    updated kafka sceanrio

--- kafka-connector/background.sh
+++ kafka-connector/background.sh
@@ -3,7 +3,11 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
 wget http://apache.spinellicreations.com/kafka/2.2.2/kafka_2.11-2.2.2.tgz
 mkdir kafka
 tar -xzf kafka_2.11-2.2.2.tgz -C kafka --strip-components=1
-wget http://downloads.datastax.com/enterprise/dse-6.8.tar.gz
+wget https://downloads.datastax.com/enterprise/dse-6.8.tar.gz
 tar xzf dse-6.8.tar.gz
-dse-6.8/bin/cassandra -R
+sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' dse-6.8.0/resources/cassandra/conf/cassandra.yaml
+dse-6.8.0/bin/dse cassandra -R
+while [ `grep "Starting listening for CQL clients" /var/log/cassandra/system.log | wc -l` -lt 1 ]; do
+ sleep 15
+done
 echo "done" >> /opt/katacoda-background-finished
