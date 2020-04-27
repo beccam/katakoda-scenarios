c0717759777e44f3a2ddfa1318fac0fba8cb5df5
Rebecca Mills <beccamills86@gmail.com>
2020-04-27 14:21:54 -0230

    updated files

--- jshell/background.sh
+++ jshell/background.sh
@@ -16,4 +16,6 @@ apache-cassandra-4.0-alpha4/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha4/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
+apache-cassandra-4.0-alpha4/bin/cqlsh -e "CREATE KEYSPACE demo WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
+apache-cassandra-4.0-alpha4/bin/cqlsh -e "CREATE TABLE demo.users (lastname text PRIMARY KEY, firstname text, email text);"
 echo "done" >> /opt/katacoda-background-finished
