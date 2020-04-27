707aed01589fa227b053d0f86959c46e6c71715f
Rebecca Mills <beccamills86@gmail.com>
2020-04-27 13:11:22 -0230

    updated files

--- jshell/background.sh
+++ jshell/background.sh
@@ -9,11 +9,11 @@ cd ..
 apt-get update
 apt install -y openjdk-11-jdk-headless
 
-wget https://archive.apache.org/dist/cassandra/4.0-alpha3/apache-cassandra-4.0-alpha3-bin.tar.gz
-tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
-apache-cassandra-4.0-alpha3/bin/cassandra -R
+https://apache.mirror.gtcomm.net/cassandra/4.0-alpha4/apache-cassandra-4.0-alpha4-bin.tar.gz
+tar xzf apache-cassandra-4.0-alpha4-bin.tar.gz
+apache-cassandra-4.0-alpha4/bin/cassandra -R
 
-while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha3/logs/system.log | wc -l` -lt 1 ]; do
+while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha4/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
 echo "done" >> /opt/katacoda-background-finished
