3d9b28ffbceca265732b75e22ab706bc8b1698dd
Rebecca Mills <beccamills86@gmail.com>
2020-05-04 11:08:32 -0230

    updated step1

--- try-java/background.sh
+++ try-java/background.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64" > .bashrc
 
 touch .mavenrc
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64" > .mavenrc
