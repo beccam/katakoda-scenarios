067149f2524e5109a9aa6d98bebc1cd899f3dc7a
Rebecca Mills <beccamills86@gmail.com>
2020-05-04 14:17:26 -0230

    updated try-java

--- try-java/background.sh
+++ try-java/background.sh
@@ -9,11 +9,11 @@ cd ..
 apt-get update
 apt install -y openjdk-11-jdk-headless
 
-wget https://archive.apache.org/dist/cassandra/4.0-alpha3/apache-cassandra-4.0-alpha3-bin.tar.gz
-tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
-apache-cassandra-4.0-alpha3/bin/cassandra -R
+wget http://apache.forsale.plus/cassandra/4.0-alpha4/apache-cassandra-4.0-alpha4-bin.tar.gz
+tar xzf apache-cassandra-4.0-alpha4-bin.tar.gz
+apache-cassandra-4.0-alpha4/bin/cassandra -R
 
-while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha3/logs/system.log | wc -l` -lt 1 ]; do
+while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha4/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
