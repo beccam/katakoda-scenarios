f8ea07e47f57f458c26bc41ee288d31657da2482
Rebecca Mills <beccamills86@gmail.com>
2020-05-04 09:54:01 -0230

    updated background.sh

--- try-java/background.sh
+++ try-java/background.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-mkdir quickstart
-mkdir quickstart/src
-mkdir quickstart/src/main
-mkdir quickstart/src/main/java
-mkdir quickstart/src/main/resources
-wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
-tar xzf apache-cassandra-3.11.6-bin.tar.gz
-sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' apache-cassandra-3.11.6/conf/cassandra.yaml
-apache-cassandra-3.11.6/bin/cassandra -R
-while [ `grep "Starting listening for CQL clients" apache-cassandra-3.11.6/logs/system.log | wc -l` -lt 1 ]; do
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+
+git clone https://github.com/DataStax-Examples/quickstart-java.git
+cd quickstart-java
+mvn compile
+cd ..
+
+apt-get update
+apt install -y openjdk-11-jdk-headless
+
+wget https://archive.apache.org/dist/cassandra/4.0-alpha3/apache-cassandra-4.0-alpha3-bin.tar.gz
+tar xzf apache-cassandra-4.0-alpha3-bin.tar.gz
+apache-cassandra-4.0-alpha3/bin/cassandra -R
+
+while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha3/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
-apache-cassandra-3.11.6/bin/cqlsh -e "CREATE KEYSPACE demo WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-apache-cassandra-3.11.6/bin/cqlsh -e "CREATE TABLE demo.users (lastname text PRIMARY KEY, firstname text, email text);"
 echo "done" >> /opt/katacoda-background-finished
